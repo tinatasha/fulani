c559a805f6b642652bfb610d72d1b27621fc4ce5
tinatasha <tashambiti@gmail.com>
2019-10-30 10:07:31 +0300

    add content in new files

--- feed/urls.py
+++ feed/urls.py
@@ -6,6 +6,9 @@ from django.shortcuts import redirect
 
 urlpatterns = [
     url(r'login/edit_profile/feed/', views.feed, name='feed'),
+    url(r'login/edit_profile/feed/briefcase', views.briefcase, name='briefcase'),
+    url(r'login/edit_profile/feed/medkit', views.medkit, name='medkit'),
+    url(r'login/edit_profile/feed/police', views.police, name='police'),
     url(r'signup/', views.signup, name='signup'),
     url(r'login/profile/', views.profile, name="profile"),
     url(r'login/edit_profile/', views.edit_profile, name="edit_profile"),
